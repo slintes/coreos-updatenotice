18a3ed23c93ee38a2140392a33a7f436ec51b0c5
Vincent De Smet <vincent.desmet@honestbee.com>
2016-12-22 20:57:24 +0800

    Add proper logger to echo-server

--- echo-server/server.py
+++ echo-server/server.py
@@ -1,19 +1,30 @@
 #!/usr/bin/env python
 from flask import Flask, url_for, json, request
+import logging
 app = Flask(__name__)
+
+@app.before_first_request
+def init():
+    app.logger.addHandler(logging.StreamHandler())
+    app.logger.setLevel(logging.INFO)
 
 @app.route('/')
 def api_root():
+    app.logger.info("OK")
     return 'OK'
 
 @app.route('/echo', methods = ['POST'])
 def api_echo():
     if request.headers['Content-Type'] == 'text/plain':
-        print request.data
+        app.logger.info(request.data)
         return request.data
     if request.headers['Content-Type'] == 'application/json':
-        print json.dumps(request.json)
+        if request.json is None:
+            app.logger.info("empty json")
+        else:
+            app.logger.info(json.dumps(request.json))
         return json.dumps(request.json)
+    app.logger.error("unsupported content type")
     return 'unsupported Content-Type'
 
 if __name__ == '__main__':
